a5d4cf8b13e87e44acd2f5f1284b19aa5b1d170c
StarkOdinson612 <darkknightishaan@gmail.com>
2024-02-09 12:14:23 -0600

    Added hood simulation

---
+++ src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.hood;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class HoodIOSim implements HoodIO {
+  private static final double LOOP_PERIOD_SECS = 0.02;
+
+  private DCMotorSim hoodPivotSim = new DCMotorSim(DCMotor.getNeo550(1), 1, 0.025);
+
+  private double hoodPivotAppliedVolts = 0d;
+
+  @Override
+  public void updateInputs(HoodIOInputs inputs) {
+    hoodPivotSim.update(LOOP_PERIOD_SECS);
+
+    inputs.hoodPivotAppliedVolts = hoodPivotAppliedVolts;
+    inputs.hoodPivotVelocityRadPerSec = hoodPivotSim.getAngularVelocityRadPerSec();
+    inputs.hoodPivotCurrentAmps = new double[] {Math.abs(hoodPivotSim.getCurrentDrawAmps())};
+  }
+
+  @Override
+  public void setHoodPivotVoltage(double volts) {
+    hoodPivotAppliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+
+    hoodPivotSim.setInputVoltage(hoodPivotAppliedVolts);
+  }
+
+  @Override
+  public void stop() {
+    hoodPivotAppliedVolts = 0;
+    hoodPivotSim.setInputVoltage(hoodPivotAppliedVolts);
+  }
+}
